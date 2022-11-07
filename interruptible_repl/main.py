9dc738dc234eff1a6ec8dfbde11e710d3be137f2
nahco314 <nahco3_ta@yahoo.co.jp>
2022-11-07 10:03:55 +0900

    Support for cli executions where the source and destination files are different

--- interruptible_repl/main.py
+++ interruptible_repl/main.py
@@ -8,9 +8,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("repl_path", type=Path)
     parser.add_argument("code")
+    parser.add_argument("--output-path", "-o", default=None, type=Path)
     args = parser.parse_args()
 
-    run(args.repl_path, args.code)
+    run(args.repl_path, args.code, output_path=args.output_path)
 
 
 if __name__ == "__main__":
